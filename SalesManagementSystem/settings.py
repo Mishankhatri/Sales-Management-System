601498b03dbd5d7397dacef1a42a5e4156a9ecc8
Mishan khatri <mishankhatri490@gmail.com>
2022-01-22 17:22:31 +0545

    mysql connection created

--- SalesManagementSystem/settings.py
+++ SalesManagementSystem/settings.py
@@ -79,13 +79,17 @@ WSGI_APPLICATION = 'SalesManagementSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'salesmanagement-db',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
